ed2dc7510414e40bd14f86f34f04f42633bbc1cc
DesislavaF <156531205+DesislavaF@users.noreply.github.com>
2024-05-31 07:26:32 +0300

    Extract components from header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-
+import Header from './components/Header';
 import style from './App.module.css';
 
 function App() {
   return (
     <div className={style.app}>
-      Let`s
+      <div className={style.container}></div>
+     <Header />
     </div>
   );
 }
